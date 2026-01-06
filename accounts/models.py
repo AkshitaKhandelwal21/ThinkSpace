7fc81a5e42f3500d4a2c54fcd842768e98f5f839
AkshitaKhandelwal21 <khandelwalakshita041@gmail.com>
2026-01-06 13:40:10 +0530

    Models made

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    class RoleChoices(models.TextChoices):
+        ADMIN = "admin"
+        USER = "user"
+    
+    role = models.CharField(max_length=5, choices=RoleChoices.choices, default=RoleChoices.USER)
+
+    def is_admin(self):
+        return self.role == self.RoleChoices.ADMIN
